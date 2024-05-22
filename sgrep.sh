62c5a57aaf5c04efeec76de80bfb0f413a54e30e
dawid993 <bieniekdawid2aizst@gmail.com>
2024-05-22 14:35:23 +0200

    Updated solution

--- sgrep.sh
+++ sgrep.sh
@@ -1,84 +1,67 @@
 #!/bin/bash
-
-print_arr() {
-        local arr_name=$1
-        eval "local arr_elems=(\"\${${arr_name}[@]}\")"
-        for elem in "${arr_elems[@]}"; do
-                echo "$elem"
-        done;
+to_lower() {
+        echo "$1" | tr "[:upper:]" "[:lower:]"
 }
-
-search_term=$1
-flag_param_index=-1
-
-if [ $# -lt 3 ]; then
-        echo "sgrip search_term [flags] files"
-        exit 1
-fi
-
-# Parsing input to retrieve flag arguments
-
+is_match() {
+        local match="false"
+        local line=$1
+        local search_term=$2
+        if [[ "$v_flag" == "true" ]]; then
+                match=$( [[ ! "$line" =~ $search_term ]] && echo "true" || echo "false")
+        elif [[ "$x_flag" == "true" ]]; then
+                match=$( [[ "$line" == "$search_term" ]] && echo "true" || echo "false")
+        else
+                match=$( [[ "$line" =~ $search_term ]] && echo "true" || echo "false")
+        fi
+        echo "$match"
+}
+grep_file() {
+        local file=$1
+        counter=1
+        while IFS= read -r line; do
+                orig_line=$line
+                if [[ "$i_flag" == "true" ]]; then
+                        line=$(to_lower "$line")
+                        search_term=$(to_lower "$search_term")
+                fi
+                if [[ "$(is_match "$line" "$search_term")"  == "true" ]]; then
+                        if [[ "$l_flag" == "true" ]]; then
+                                echo "$file"
+                                break;
+                        fi
+                        out=""
+                        if [[ ${#files[@]} -gt 1 ]]; then
+                                out+="$file:"
+                        fi
+                        if [[ "$n_flag" == "true" ]]; then
+                                out+="$counter:"
+                        fi
+                        out+="$orig_line"
+                        echo "$out"
+                fi
+                counter=$((counter+1))
+        done < "$file"
+}
+#Parsing input to retrieve flag arguments
 n_flag=false
 l_flag=false
-
-for (( i=2; i<=$#; i=i+1 )); do
-        arg="${!i}"
-        if [[ $arg != -* ]]; then
-                break
-        fi
-
-        if [[ "$arg" == "-n" ]]; then
-                n_flag=true
-        elif [[ "$arg" == "-l" ]]; then
-                l_flag=true
-        fi
-
-        flag_param_index=$i
+i_flag=false
+v_flag=false
+x_flag=false
+while [[ "$1" == -* ]]; do
+        case "$1" in
+                -n) n_flag=true ;;
+                -l) l_flag=true ;;
+                -i) i_flag=true ;;
+                -v) v_flag=true ;;
+                -x) x_flag=true ;;
+        esac
+        shift
 done
-
-# Build files arrays
-if [ $flag_param_index -eq -1 ]; then
-        files=("${@:2}")
-else
-        files=("${@:$((flag_param_index+1))}")
-fi
-
-
-if [ ${#files[@]} -eq 0 ]; then
-        echo "Missing files argument: sgrip search_term [flags] files"
-        exit 1
-fi
-
-# Nice to debug input
-# echo "Files: ${files[@]}"
-# echo "Number of files: ${#files[@]}"
-
-results=()
-files_results=()
-
-for file in ${files[@]}; do
-        counter=0
-        while IFS= read -r line; do
-                if [[ $line =~ $search_term ]]; then
-                        if [[ "$n_flag" == "true" ]]; then
-                                results+=("$file:$counter:$line")
-                        else
-                                results+=("$line")
-                        fi
-
-                        if [[ "$l_flag" == "true" ]]; then
-                                files_results+=("$file")
-                        fi
-
-                fi
-        done < "$file"
+search_term="$1"
+shift
+files=("$@")
+for file in "${files[@]}"; do
+        grep_file "$file"
 done
-
-#echo "Results: ${results[@]}"
-if [[ "$l_flag" == "true" ]]; then
-        print_arr "files_results"
-else
-        print_arr "results"
-fi
-
 exit 0
